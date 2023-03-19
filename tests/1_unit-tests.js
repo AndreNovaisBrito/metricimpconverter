bc726084b81ffa8741d231f6b39a972651651cb9
AndreNovaisBrito <andrenoz@gmail.com>
2023-03-19 09:23:43 -0300

    Unit tests organized

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -9,9 +9,11 @@ suite("Unit Tests", function () {
     test("Whole number", function () {
       assert.strictEqual(convertHandler.getNum("32L"), 32);
     });
+
     test("Decimal Number", () => {
       assert.strictEqual(convertHandler.getNum("3.1L"), 3.1);
     });
+
     test("Fractional Number", () => {
       assert.strictEqual(convertHandler.getNum("1/2mi"), 0.5);
     });
@@ -19,66 +21,104 @@ suite("Unit Tests", function () {
     test("Fractional Number With Decimal", () => {
       assert.strictEqual(convertHandler.getNum("1.5/3mi"), 0.5);
     });
+
     test("Double Fraction Error", () => {
       assert.strictEqual(convertHandler.getNum("1/1/1"), "invalid number");
     });
+
     test("No number input defaults to 1", () => {
       assert.strictEqual(convertHandler.getNum("mi"), 1);
     });
-    test("Read All Units", () => {
+
+    test("Read Unit for gallons", () => {
       assert.strictEqual(convertHandler.getUnit("22gal"), "gal");
+    });
+
+    test("Read Unit for pounds", () => {
       assert.strictEqual(convertHandler.getUnit("22lbs"), "lbs");
+    });
+
+    test("Read Unit for miles", () => {
       assert.strictEqual(convertHandler.getUnit("22mi"), "mi");
+    });
+
+    test("Read Unit for liters", () => {
       assert.strictEqual(convertHandler.getUnit("22l"), "L");
+    });
+
+    test("Read Unit for kilograms", () => {
       assert.strictEqual(convertHandler.getUnit("22kg"), "kg");
+    });
+
+    test("Read Unit for kilometers", () => {
       assert.strictEqual(convertHandler.getUnit("22km"), "km");
     });
-    test("Read SpellOut Units", () => {
+
+    test("Spell out units for gallons", () => {
       assert.strictEqual(convertHandler.spellOutUnit("gal"), "gallons");
+    });
+
+    test("Spell out units for pounds", () => {
       assert.strictEqual(convertHandler.spellOutUnit("lbs"), "pounds");
+    });
+
+    test("Spell out units for miles", () => {
       assert.strictEqual(convertHandler.spellOutUnit("mi"), "miles");
+    });
+
+    test("Spell out units for liters", () => {
       assert.strictEqual(convertHandler.spellOutUnit("l"), "liters");
+    });
+
+    test("Spell out units for kilograms", () => {
       assert.strictEqual(convertHandler.spellOutUnit("kg"), "kilograms");
+    });
+
+    test("Spell out units for kilometers", () => {
       assert.strictEqual(convertHandler.spellOutUnit("km"), "kilometers");
     });
-    test("Return Units", () => {
+
+    test("Return Unit for gallons", () => {
       assert.strictEqual(convertHandler.getReturnUnit("gal"), "L");
+    });
+
+    test("Return Unit for pounds", () => {
       assert.strictEqual(convertHandler.getReturnUnit("lbs"), "kg");
+    });
+
+    test("Return Unit for miles", () => {
       assert.strictEqual(convertHandler.getReturnUnit("mi"), "km");
+    });
+
+    test("Return Unit for liters", () => {
       assert.strictEqual(convertHandler.getReturnUnit("l"), "gal");
+    });
+
+    test("Return Unit for kilograms", () => {
       assert.strictEqual(convertHandler.getReturnUnit("kg"), "lbs");
+    });
+
+    test("Return Unit for kilometers", () => {
       assert.strictEqual(convertHandler.getReturnUnit("km"), "mi");
     });
-    test("Accept Uppercase", () => {
-        assert.strictEqual(convertHandler.getReturnUnit("GAL"), "L");
-        assert.strictEqual(convertHandler.getReturnUnit("LBS"), "kg");
-        assert.strictEqual(convertHandler.getReturnUnit("MI"), "km");
-        assert.strictEqual(convertHandler.getReturnUnit("L"), "gal");
-        assert.strictEqual(convertHandler.getReturnUnit("KG"), "lbs");
-        assert.strictEqual(convertHandler.getReturnUnit("KM"), "mi");
-      });
-      test("Convert Tests", () => {
-        assert.strictEqual(convertHandler.convert(1, "gal"), 3.78541);
-        assert.strictEqual(convertHandler.convert(1, "lbs"), 0.45359);
-        assert.strictEqual(convertHandler.convert(1, "mi"), 1.60934);
-        assert.strictEqual(convertHandler.convert(1, "l"), 0.26417);
-        assert.strictEqual(convertHandler.convert(1, "kg"), 2.20462);
-        assert.strictEqual(convertHandler.convert(1, "km"), 0.62137);
-      });
-      test("Whole number 2", function () {
-        assert.strictEqual(convertHandler.getNum("32L"), 32);
-      });
-      test("Whole number 3", function () {
-        assert.strictEqual(convertHandler.getNum("32L"), 32);
-      });
-      test("Whole number 4", function () {
-        assert.strictEqual(convertHandler.getNum("32L"), 32);
-      });
-      test("Whole number 5", function () {
-        assert.strictEqual(convertHandler.getNum("32L"), 32);
-      });
-      test("Whole number 6", function () {
-        assert.strictEqual(convertHandler.getNum("32L"), 32);
-      });
+    test("Convert 1 gallon to liters", () => {
+      assert.approximately(convertHandler.convert(1, "gal"), 3.78541, 0.1);
+    });
+
+    test("Convert 1 pound to kilograms", () => {
+      assert.approximately(convertHandler.convert(1, "lbs"), 0.45359, 0.1);
+    });
+
+    test("Convert 1 mile to kilometers", () => {
+      assert.approximately(convertHandler.convert(1, "mi"), 1.60934, 0.1);
+    });
+
+    test("Convert 1 liter to gallons", () => {
+      assert.approximately(convertHandler.convert(1, "l"), 0.26417, 0.1);
+    });
+
+    test("Convert 1 kilogram to pounds", () => {
+      assert.approximately(convertHandler.convert(1, "kg"), 2.20462, 0.1);
+    });
   });
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,21 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+    describe("GET /api/convert", () => {
+        it("should convert a valid input such as 10L", (done) => {
+          chai
+            .request(server)
+            .get("/api/convert")
+            .query({ input: "10L" })
+            .end((err, res) => {
+              assert.equal(res.status, 200);
+              assert.equal(res.body.initNum, 10);
+              assert.equal(res.body.initUnit, "L");
+              assert.approximately(res.body.returnNum, 2.64172, 0.1);
+              assert.equal(res.body.returnUnit, "gal");
+              assert.equal(res.body.string, "10 liters converts to 2.64172 gallons");
+              done();
+            });
+        });
+    });
 });
